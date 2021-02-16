0702430accedb5e143aff3790f5d7883aeeb74c2
Jacob <you@example.com>
2021-02-16 23:19:57 +0100

    test

---
+++ src/main/java/dtos/MovieDTO.java
@@ -0,0 +1,100 @@
+package dtos;
+
+import entities.Movie;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MovieDTO {
+    private long id;
+    private String title;
+    private int year;
+    private String[] actors;
+
+    public MovieDTO(String title, int year, String[] actors) {
+        this.title = title;
+        this.year = year;
+        this.actors = actors;
+    }
+    public MovieDTO(Movie m) {
+        this.id = m.getId();
+        this.title = m.getTitle();
+        this.year = m.getYear();
+        this.actors = m.getActors();
+    }
+        
+    public static List<MovieDTO> getMovieDTOs(List<Movie> movies){
+        List<MovieDTO> movieDTOs = new ArrayList();
+        movies.forEach(movie -> movieDTOs.add(new MovieDTO(movie)));
+        return movieDTOs;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String[] getActors() {
+        return actors;
+    }
+
+    public void setActors(String[] actors) {
+        this.actors = actors;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + (int) (this.id ^ (this.id >>> 32));
+        hash = 97 * hash + Arrays.deepHashCode(this.actors);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final MovieDTO other = (MovieDTO) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Arrays.deepEquals(this.actors, other.actors)) {
+            return false;
+        }
+        return true;
+    }
+
+
+    
+    
+    
+    
+    
+    
+}
